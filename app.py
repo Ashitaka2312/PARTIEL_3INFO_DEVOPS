54408dd093986db8be9bb82769a6d92530c8bb90
ashitaka <pcid@myges.fr>
2025-02-26 12:14:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,16 @@ def add(a, b):
     """Ajoute deux nombres."""
     return a + b
 
+
 def multiply( x,y ):
     return x*y
+
 
 def divide(x , y ):
     beta = 0
     if y != 0:
         return x/y
+
 
 def greet(name):
     # GREET FONCTION
